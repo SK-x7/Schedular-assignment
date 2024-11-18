7c527ed9f174373e34182e60ccf430ba3a3ab68b
SK-x7 <111245838+SK-x7@users.noreply.github.com>
2024-11-19 02:13:11 +0530

    completed booking cards in my bookings page

--- frontend/src/bookings/components/AllBookings.jsx
+++ frontend/src/bookings/components/AllBookings.jsx
@@ -13,7 +13,7 @@ function AllBookings() {
                 {
                     allBookings&&allBookings.map((booking,i)=>(
                         // <Link to={`/create-booking/${booking?.instructorId}/${booking?.id}`} key={i} state={{booking}} onClick={()=>localStorage.setItem("currentEventDuration",booking?.duration)}>
-                        <div className="bg-white rounded-lg min-h-64 h-min max-h-72 px-5 py-5 flex flex-col gap-3 justify-start" key={i}>
+                        <div className="bg-white rounded-lg min-h-64 h-min max-h-72 px-5 py-5 flex flex-col justify-between items-start" key={i}>
                         <div className="flex flex-col gap-2">
                             <div className="flex justify-between items-center">
                                 <h1 className="text-2xl line-clamp-1">{booking?.title||"This is tytle"}</h1>
@@ -23,17 +23,17 @@ function AllBookings() {
                             <span className="text-gray-600 text-xs">|</span>
                             <span className="text-gray-600 text-xs">{booking?.duration||"30"} mins</span>
                         </div>
-                        <p className="!text-xs line-clamp-[7]">&quot;{booking?.descriptin||"This is duration This is duration This is duration This is duration This is duration This is duration This is duration This is duration This is durationduration This is durationduration This is durationduration This is durationduration This is durationdurationduration This is durationduration This is durationduration This is durationduration This is durationduration This is durationdurationduration This is durationduration This is durationduration This is durationduration This is durationduration This is durationdurationduration This is durationduration This is durationduration This is durationduration This is durationduration This is durationdurationduration This is durationduration This is durationduration This is durationduration This is durationduration This is durationdurationduration This is durationduration This is durationduration This is durationduration This is durationduration This is durationdurationduration This is durationduration This is durationduration This is durationduration This is durationduration This is durationduration This is durationduration This ration This ration This ration This ration This ration This ration This ration This ration This ration This ration This ration This ration This ration This ration This ration This ration This is durationduration This is durationduration This is durationduration This is durationduration This is duration"}&quot;</p>
+                        <p className="!text-xs line-clamp-[7] first-letter:capitalize">&quot;{booking?.description||"This is duration"}&quot;</p>
                         </div>
                         <div className="flex flex-col gap-[2px]">
                             <span className="text-sm">📅 : {booking?.startTime.toString().slice(8,10)}-{booking?.startTime.toString().slice(5,7)}-{booking?.startTime.toString().slice(0,4)}</span>
                             <span className="text-sm">🕰️ : {(booking?.startTime).toString().slice(11,16)} - {(booking?.endTime).toString().slice(11,16)}</span>
                             
                         </div>
-                        <div className="flex gap-4">
-                        {/* <button className="bg-blue-600 text-white font-semibold rounded-lg px-3 py-[6px]">Copy link</button> */}
-                        {/* <button className="bg-red-600 text-white font-semibold rounded-lg px-3 py-[6px] ">Delete</button> */}
-                        </div>
+                            <div>
+                                
+                            <button className="capitalize flex justify-center items-center bg-red-500 text-white font-semibold py-1 px-3 rounded-lg">Cancel booking</button>
+                            </div>
                     </div>
                 // </Link>
                     ))
